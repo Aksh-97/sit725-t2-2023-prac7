acab56387f6b969b28c2623250631528a2fd4776
Aksh-97 <101445571+Aksh-97@users.noreply.github.com>
2023-09-04 20:39:44 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+let express = require('express');
+let app = express();
+
+
+let port = process.env.port || 3000;
+
+require('./dbConnection');
+let router= require('./routers/router')
+const { Socket } = require('socket.io');
+let http = require('http').createServer(app);
+
+
+
+
+app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api/cat',router);
+
+let io = require('socket.io')(http);
+io.on('connection',(socket)=>{
+  console.log('something');
+  socket.on('disconnect', () => {
+      console.log('user disconnected');
+  });
+
+  setInterval(()=>{
+      socket.emit('number', parseInt(Math.random()*10));
+  }, 1000)
+});
+
+
+http.listen(port, () => {
+  console.log(" SERVER HAS STARTED and APP is listening to http://localhost:" + port);
+ ;
+});
